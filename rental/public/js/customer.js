474060a97429e8894d60c64a372420f71557c004
viral <viral@fosserp.com>
2026-08-04 05:01:37 +0000

    Remove Customer form's Create Subscription button
    Found while auditing the previous Subscription-disabling commit:
    customer.js still added a "Create Subscription" button calling
    rental.api.create_subscription, which was commented out in that same
    change. Clicking it would have thrown a server-side error since the
    whitelisted method no longer exists.
    
    Commented out the button, consistent with the rest of the Subscription
    logic removal.

--- rental/public/js/customer.js
+++ rental/public/js/customer.js
@@ -14,12 +14,13 @@ frappe.ui.form.on('Customer', {
                 }
             };
         });
-        frm.add_custom_button(__('Create Subscription'), function () {
-            frappe.model.open_mapped_doc({
-                method: "rental.api.create_subscription",
-                frm: frm
-            })
-        },__('Create'));
+        // Subscription is no longer created or used in this app (requirement change).
+        // frm.add_custom_button(__('Create Subscription'), function () {
+        //     frappe.model.open_mapped_doc({
+        //         method: "rental.api.create_subscription",
+        //         frm: frm
+        //     })
+        // },__('Create'));
         frm.remove_custom_button(__("Pricing Rule") , __("Create"))
     },
     custom_agreement_start_date:function(frm){
